b17522372e0a7758c656f759240a5c48c6cb4429
Kevin Tan <kevin.tan97@hotmail.com>
2018-12-14 21:58:09 -0500

    added code to store all tickers currently on wiki page

--- data-ms/sp500/list_scrapper.py
+++ data-ms/sp500/list_scrapper.py
@@ -1,6 +1,7 @@
 import bs4 as bs
 import pickle
 import requests
+import psycopg2
 
 def pull_sp500_stocks():
     resp = requests.get('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
@@ -12,7 +13,21 @@ def pull_sp500_stocks():
         tickers.append(row.findAll('td')[0].text)
     return tickers
 
-# TODO: create table for s&p 500 index
-pull_sp500_stocks()
+def store_to_db(tickers):
+    conn = psycopg2.connect('dbname=ModelData user=postgres password=123')
+    cur = conn.cursor()
 
+    # Drop and recreate table for updating tickers
+    cur.execute("DROP TABLE SP500;")
+    cur.execute("CREATE TABLE SP500 (ticker)")
+
+     # Add to db
+    for ticker in tickers:
+        cur.execute(f"INSERT INTO SP500 (ticker) VALUES ('{ticker}');")
+    conn.commit()
+    cur.close()
+    conn.close()
+
+# This file should be called whenever there is a need to update the tickers
+store_to_db(pull_sp500_stocks())
 
